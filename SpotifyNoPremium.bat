f97504b2446fc52ecd505a30fa7bfeec5524740e
Daksh P. Jain <jaindaksh42@gmail.com>
2021-07-08 17:36:34 +0530

    Update SpotifyNoPremium.bat

--- SpotifyNoPremium.bat
+++ SpotifyNoPremium.bat
@@ -22,25 +22,6 @@ Write-Host 'Stopping Spotify...'`n
 Stop-Process -Name Spotify
 Stop-Process -Name SpotifyWebHelper
 
-if (Get-AppxPackage -Name SpotifyAB.SpotifyMusic) {
-  Write-Host @'
-The Microsoft Store version of Spotify has been detected which is not supported.
-'@`n
-  $ch = Read-Host -Prompt "Uninstall Spotify Windows Store edition (Y/N) "
-  if ($ch -eq 'y'){
-     Write-Host @'
-Uninstalling Spotify.
-'@`n
-     Get-AppxPackage -Name SpotifyAB.SpotifyMusic | Remove-AppxPackage
-  } else{
-     Write-Host @'
-Exiting...
-'@`n
-     Pause 
-     exit
-    }
-}
-
 Push-Location -LiteralPath $env:TEMP
 try {
   # Unique directory name based on time
@@ -69,36 +50,6 @@ try {
 
 Expand-Archive -Force -LiteralPath "$PWD\chrome_elf.zip" -DestinationPath $PWD
 Remove-Item -LiteralPath "$PWD\chrome_elf.zip"
-
-$spotifyInstalled = (Test-Path -LiteralPath $SpotifyExecutable)
-if (-not $spotifyInstalled) {
-  Write-Host @'
-Spotify installation was not detected.
-Downloading Latest Spotify full setup, please wait...
-'@
-  try {
-    $webClient.DownloadFile(
-      # Remote file URL
-      'https://download.scdn.co/SpotifyFullSetup.exe',
-      # Local file path
-      "$PWD\SpotifyFullSetup.exe"
-    )
-  } catch {
-    Write-Output $_
-    Pause
-    exit
-  }
-  mkdir $SpotifyDirectory >$null 2>&1
-  Write-Host 'Running installation...'
-  Start-Process -FilePath "$PWD\SpotifyFullSetup.exe"
-  Write-Host 'Stopping Spotify...Again'
-  while ((Get-Process -name Spotify -ErrorAction SilentlyContinue) -eq $null){
-     #waiting until installation complete
-     }
-  Stop-Process -Name Spotify >$null 2>&1
-  Stop-Process -Name SpotifyWebHelper >$null 2>&1
-  Stop-Process -Name SpotifyFullSetup >$null 2>&1
-}
 
 if (!(test-path $SpotifyDirectory/chrome_elf.dll.bak)){
 	move $SpotifyDirectory\chrome_elf.dll $SpotifyDirectory\chrome_elf.dll.bak >$null 2>&1
