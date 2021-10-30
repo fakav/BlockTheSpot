6a9ae36bb198abe4ac8441ce09cdf2371d26d7eb
Daksh P. Jain <jaindaksh42@gmail.com>
2021-10-30 09:17:38 +0530

    Update SpotifyNoPremium.ps1

--- SpotifyNoPremium.ps1
+++ SpotifyNoPremium.ps1
@@ -9,13 +9,20 @@ Modified By: @Daksh777
 
 $SpotifyDirectory = "$env:APPDATA\Spotify"
 $SpotifyExecutable = "$SpotifyDirectory\Spotify.exe"
+$SpotifyApps = "$SpotifyDirectory\Apps"
 
 Write-Host 'Stopping Spotify...'`n
 Stop-Process -Name Spotify
 Stop-Process -Name SpotifyWebHelper
 
+if ($PSVersionTable.PSVersion.Major -ge 7)
+{
+    Import-Module Appx -UseWindowsPowerShell
+}
+
 Push-Location -LiteralPath $env:TEMP
 try {
+  # Unique directory name based on time
   New-Item -Type Directory -Name "BlockTheSpot-$(Get-Date -UFormat '%Y-%m-%d_%H-%M-%S')" `
   | Convert-Path `
   | Set-Location
@@ -29,23 +36,26 @@ Write-Host 'Downloading latest patch (chrome_elf.zip)...'`n
 $webClient = New-Object -TypeName System.Net.WebClient
 try {
   $webClient.DownloadFile(
+    # Remote file URL
     'https://github.com/mrpond/BlockTheSpot/releases/latest/download/chrome_elf.zip',
+    # Local file path
     "$PWD\chrome_elf.zip"
   )
 } catch {
   Write-Output $_
-  Start-Sleep
+  Sleep
 }
 
 Expand-Archive -Force -LiteralPath "$PWD\chrome_elf.zip" -DestinationPath $PWD
 Remove-Item -LiteralPath "$PWD\chrome_elf.zip"
 
-if (!(test-path $SpotifyDirectory/chrome_elf.dll.bak)){
-	Move-Item $SpotifyDirectory\chrome_elf.dll $SpotifyDirectory\chrome_elf.dll.bak >$null 2>&1
+if (!(test-path $SpotifyDirectory/chrome_elf_bak.dll)){
+	move $SpotifyDirectory\chrome_elf.dll $SpotifyDirectory\chrome_elf_bak.dll >$null 2>&1
 }
 
 Write-Host 'Patching Spotify...'
 $patchFiles = "$PWD\chrome_elf.dll", "$PWD\config.ini"
+
 Copy-Item -LiteralPath $patchFiles -Destination "$SpotifyDirectory"
 
 $tempDirectory = $PWD
